fd33089c1db35d4bdf6a460f486b9442ff015f27
khanhnhan1512 <94161431+khanhnhan1512@users.noreply.github.com>
2023-10-21 14:48:09 +0700

    running time by python

--- main.py
+++ main.py
@@ -13,12 +13,22 @@ def multiply_matrix(matrixA, matrixB):
         return None
     return [[sum(i * j for i, j in zip(row, col)) for col in zip(*matrixB)] for row in matrixA]
 
+def record_running_time(matrix_size):
+    with open('Time/python.txt') as f:
+        for size in matrix_size:
+            matrixA = read_matrix(f'Matrix Folder/matrix_A_{size}.txt')
+            matrixB = read_matrix(f'Matrix Folder/matrix_B_{size}.txt')
+            start = time.time()
+            result = multiply_matrix(matrixA, matrixB)
+            end = time.time()
+            running_time = end - start
+            print(f'Matrix {size}: {running_time}')
+            f.write(f'{running_time}\n')
+    f.close()
+
 def main():
     matrix_size = [10, 50, 100, 250, 500, 750, 1000]
-    matrixA = read_matrix('Matrix Folder/matrix_A_10.txt')
-    matrixB = read_matrix('Matrix Folder/matrix_B_10.txt')
-    result = multiply_matrix(matrixA, matrixB)
-    print(result)
+    
 
 if __name__ == '__main__':
     main()
